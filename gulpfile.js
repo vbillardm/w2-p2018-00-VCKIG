98c45e5fbb99b82fc8d74322f31a7b081add7802
cbecret <cyril.becret@gmail.com>
2017-02-18 17:16:08 +0100

    global[gulpfile:js] Fixing gulp for html watch

--- gulpfile.js
+++ gulpfile.js
@@ -11,20 +11,28 @@ let gulp        = require('gulp'),
     sync        = require('browser-sync').create();
 
 // Static Server + watching scss/html files
-gulp.task('serve', ['sass', 'js'], function() {
+gulp.task('serve', ['html', 'sass', 'js'], function() {
 
     sync.init({
-        server: './'
+        server: './dist/'
     });
 
-    gulp.watch('src/scss/**/*.scss', ['sass']);
-    gulp.watch('src/js/**/*.js', ['js']);
-    gulp.watch('./*.html').on('change', sync.reload);
+    gulp.watch('app/*.html', ['html']);
+    gulp.watch('app/scss/**/*.scss', ['sass']);
+    gulp.watch('app/js/**/*.js', ['js']);
 });
+
+// Configure HTML tasks.
+gulp.task('html', function () {
+  return gulp.src('app/*.html')
+    .pipe(gulp.dest('dist'))
+    .pipe(sync.stream());
+});
+
 
 // Configure CSS tasks.
 gulp.task('sass', function () {
-  return gulp.src('src/scss/**/*.scss')
+  return gulp.src('app/scss/**/*.scss')
     .pipe(sass.sync().on('error', sass.logError))
     .pipe(prefix('last 2 versions'))
     .pipe(cssmin())
@@ -35,7 +43,7 @@ gulp.task('sass', function () {
 
 // Configure JS.
 gulp.task('js', function() {
-  return gulp.src('src/js/**/*.js')
+  return gulp.src('app/js/**/*.js')
     .pipe(uglify())
     .pipe(concat('app.js'))
     .pipe(rename({suffix: '.min'}))
@@ -45,15 +53,15 @@ gulp.task('js', function() {
 
 // Configure image stuff.
 gulp.task('images', function () {
-  return gulp.src('src/img/**/*.+(png|jpg|gif|svg)')
+  return gulp.src('app/img/**/*.+(png|jpg|gif|svg)')
     .pipe(imagemin())
     .pipe(gulp.dest('dist/img'));
 });
 
 gulp.task('watch', function () {
-  gulp.watch('src/scss/**/*.scss', ['sass']);
-  gulp.watch('src/js/**/*.js', ['js']);
-  gulp.watch('./*.html').on('change', sync.reload);
+    gulp.watch('app/*.html', ['html']);
+    gulp.watch('app/scss/**/*.scss', ['sass']);
+    gulp.watch('app/js/**/*.js', ['js']);
 });
 
-gulp.task('default', ['sass', 'js', 'images', 'serve']);
+gulp.task('default', ['html', 'sass', 'js', 'images', 'serve']);
