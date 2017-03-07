5d224224dde710af74e8cc0800e3691c13d0a831
kevin nabeth <kgnabeth@gmail.com>
2017-03-07 17:44:24 +0100

    init

--- gulpfile.js
+++ gulpfile.js
@@ -1,81 +1,49 @@
-'use strict';
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var minifycss = require('gulp-minify-css');
+var sync = require('browser-sync');
+var connect = require('gulp-connect-php');
+var babel = require('gulp-babel');
+var browserify = require('gulp-browserify');
 
-let gulp = require('gulp');
-let cssmin = require('gulp-cssmin');
-let prefix = require('gulp-autoprefixer');
-let uglify = require('gulp-uglify');
-let concat = require('gulp-concat');
-let imagemin = require('gulp-imagemin');
-let sync = require('browser-sync').create();
-let sugarss = require('sugarss');
-let pixrem  = require('pixrem');
-let postcss = require('gulp-postcss');
-let cssimport = require('postcss-import');
-let cssnext = require('postcss-cssnext');
-let rename = require('gulp-rename');
-let nested = require('postcss-nested');
+gulp.task('scss', function (){
+			gulp.src(['src/scss/app.scss'])
+				.pipe(sass())
+				.pipe(gulp.dest('dist/css'))
+				.pipe(minifycss())
+				sync.reload();
+		});
 
-// Static Server + watching scss/html files
-gulp.task('serve', ['html', 'sss', 'js'], function() {
 
-    sync.init({
-        server: './dist/'
+
+gulp.task('js', function(){
+	gulp.src(['src/js/app.js'])
+		.pipe(babel({
+			presets: ['es2015']
+		}))
+		.pipe(browserify({
+			insertGlobals : true
+		}))
+		.pipe(gulp.dest('dist/js'));
+
+	setTimeout(function(){
+		sync.reload();
+	}, 500);
+
+})
+
+gulp.task('sync', ['scss'], function() {
+  connect.server({}, function (){
+    sync({
+      proxy: 'localhost:8888',
+		port: 8888//TO EDIT
     });
+  });
 
-    gulp.watch('app/*.html', ['html']);
-    gulp.watch('app/sss/**/*.sss', ['sss']);
-    gulp.watch('app/js/**/*.js', ['js']);
+    gulp.watch("src/scss/**/*.scss", ['scss']);
+	gulp.watch("src/js/**/*.js", ['js']);
 });
 
-// Configure HTML tasks.
-gulp.task('html', function () {
-  return gulp.src('app/*.html')
-    .pipe(gulp.dest('dist'))
-    .pipe(sync.stream());
-});
+gulp.task('default', ['sync'], function(){
 
-// Configure CSS tasks.
-gulp.task('sss', function () {
-  return gulp.src('app/sss/styles.sss')
-    .pipe(postcss(
-        [
-            cssimport,
-            cssnext(
-                {browsers: ['last 2 version'],
-                compress: true}
-            ),
-            pixrem,
-            nested
-        ],
-        { parser: sugarss }
-    ))
-    .pipe(concat('style'))
-    .pipe(rename({ extname: '.css' }))
-    .pipe(gulp.dest('dist/css'))
-    .pipe(sync.stream());
-});
-
-// Configure JS.
-gulp.task('js', function() {
-  return gulp.src('app/js/**/*.js')
-    .pipe(uglify())
-    .pipe(concat('app.js'))
-    .pipe(rename({suffix: '.min'}))
-    .pipe(gulp.dest('dist/js'))
-    .pipe(sync.stream());
-});
-
-// Configure image stuff.
-gulp.task('images', function () {
-  return gulp.src('app/img/**/*.+(png|jpg|gif|svg)')
-    .pipe(imagemin())
-    .pipe(gulp.dest('dist/img'));
-});
-
-gulp.task('watch', function () {
-    gulp.watch('app/*.html', ['html']);
-    gulp.watch('app/sss/**/*.sss', ['sss']);
-    gulp.watch('app/js/**/*.js', ['js']);
-});
-
-gulp.task('default', ['html', 'sss', 'js', 'images', 'serve']);
+	});
---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+include('components/header.html');
+
+?>
+
+<div class="content">
+
+    <?php include('components/section_intro.html') ?>
+    <?php include('components/section_description.html') ?>
+    <?php include('components/section_avantages.html') ?>
+    <?php include('components/section_telechargement.html') ?>
+    <?php include('components/section_contact.html') ?>
+
+</div>
+
+<?php
+include('components/footer.html');
+?>
