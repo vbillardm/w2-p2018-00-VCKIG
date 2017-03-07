6eb0e28b187269f8dd1ab4d04f9ad1a848ea5ef6
cbecret <cyril.becret@gmail.com>
2017-03-07 23:56:43 +0100

    global[app:js] Adding menu script

---
+++ app/js/app.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function(){
+
+    document.querySelector('.menu__button--open').addEventListener('click', function(){
+        var menu = document.querySelector('.header__navbar__mobile .header__navbar--list')
+        menu.style.visibility = "visible";
+        menu.style.opacity = 1;
+    });
+    document.querySelector('.menu__button--close').addEventListener('click', function(){
+        var menu = document.querySelector('.header__navbar__mobile .header__navbar--list')
+        menu.style.visibility = "hidden";
+        menu.style.opacity = 0;
+    });
+
+});
